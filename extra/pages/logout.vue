b50c41ee3831f4d07dfd19e607c915c9eaeba831
Ben Bozzay <ben@fullstackdigital.com>
2021-05-27 08:46:31 -0600

    Add boilerplate pages

---
+++ extra/pages/logout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login container w-6/12 mx-auto flex flex-wrap items-center justify-center text-center mt-0 py-32">
+    {{ loginText }}
+    <div @click="$auth.logout()" class="buttonBlack">LOGOUT</div>
+    <nuxt-link to="/login" class="buttonBlack">LOGIN</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  ssr: false,
+  data() {
+    return {
+      loggedIn: false,
+      loginText: "Already Logged Out"
+    }
+  },
+  async fetch() {
+    await this.$boss.logout()
+    this.loginText = "Logged Out"
+  },
+  fetchOnServer: false
+}
+</script>
+
+<style lang="scss">
+  @import "~/assets/scss/parts/button/black";
+  @import "~/assets/scss/parts/button/clear";
+</style>
---
+++ extra/pages/signup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login container w-6/12 mx-auto flex flex-col items-center justify-center text-center mt-0 py-32">
+    <div>
+      <h1>Select a Plan</h1>
+    </div>
+    <signup-plans :plans="plans"></signup-plans>
+  </div>
+</template>
+
+<script>
+import data from "~/data/plans";
+export default {
+  ssr: false,
+  fetchOnServer: false,
+  middleware: ["boss/requireLogin"],
+  // data() {
+  //   return {
+  // },
+  async asyncData() {
+    return {
+      // plans: dataFile.plans
+      plans: data.plans
+    }
+  },
+  async fetch() {
+    // let redirectUrl = this.$route.params.redirectUrl;
+    // redirectUrl ? redirectUrl : null;
+    // await this.$boss.loginThenRedirect(redirectUrl);
+  }
+}
+</script>
